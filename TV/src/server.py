0d866bacd8781c87717515be805e2530d10112ee
Steef Broeder <steef@broeder.co.nl>
2024-03-19 18:35:23 +0100

    fixed server

--- TV/src/server.py
+++ TV/src/server.py
@@ -3,13 +3,17 @@ import websockets
 import socket
 
 async def receive_command(websocket, path):
-    try:
-        data = await websocket.recv()
-        print(f"Data received as: {data}!")
-    except websockets.exceptions.ConnectionClosed as e:
-        print(f"Connection closed with error: {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    connected = True
+    while connected:
+        try:
+            data = await websocket.recv()
+            print(f"Data received as: {data}!")
+        except websockets.exceptions.ConnectionClosed as e:
+            print(f"Connection closed with error: {e}")
+            connected = False
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            connected = False
 
     # command = data.get('command')
         
@@ -40,11 +44,11 @@ def volume_down():
     print("Decreasing volume")
     pass
 
-ip_address = socket.gethostbyname(socket.gethostname())
-print(f"Server running on IP: {ip_address}")
+async def main(ip):
+    async with websockets.serve(receive_command, "ip", 5555):
+        await asyncio.Future()
 
-async def main():
-    async with websockets.serve(receive_command, ip_address, 5555):
-        await asyncio.Future()  # run forever
+ip_address = input("What is your local ip?")
+print(f"Server running on IP: {ip_address}:5555")
 
-asyncio.run(main())
+asyncio.run(main(ip_address))
